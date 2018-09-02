2c9b01d02fd7b503c50c56d4b0f4da87dd444923
aiturrizaga <aiturrizaga@vallegrande.edu.pe>
2018-09-02 00:16:17 -0500

    Carga de la información de Firebase a la página

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -7,21 +7,29 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada: boolean = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
     console.log('Servicio infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe((resp: InfoPagina) => {
-
         this.cargada = true;
         this.info = resp;
-
-        console.log(resp);
-
       });
+  }
 
+  private cargarEquipo() {
+    this.http.get('https://angular-html-cb8b9.firebaseio.com/equipo.json')
+      .subscribe((resp: any[]) => {
+        this.equipo = resp;
+        console.log(resp);
+      });
   }
 
 }
