161ccc821f79c17a7a56ec9ff62cb9dcda61b620
aiturrizaga <aiturrizaga@vallegrande.edu.pe>
2018-09-11 14:49:30 -0500

    Tayendo más datos de Firebase

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    constructor(public _infoService: InfoPaginaService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
