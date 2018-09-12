cd6d9afc4d89a0abe49a82849d8059ebf4ce0d05
aiturrizaga <aiturrizaga@vallegrande.edu.pe>
2018-09-11 23:29:11 -0500

    Creando el servicio de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 //Servicios
 import { InfoPaginaService } from './services/info-pagina.service';
+import { ProductosService } from './services/productos.service';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -31,7 +32,8 @@ import { ItemComponent } from './pages/item/item.component';
     HttpClientModule
   ],
   providers: [
-    InfoPaginaService
+    InfoPaginaService,
+    ProductosService
   ],
   bootstrap: [AppComponent]
 })
